9dfda664ca5eda72f96559364c952d208fdb03e6
kasutu <jeromecabugwason2@protonmail.com>
2022-12-22 18:29:17 +0800

    init

---
+++ src/main/java/com/tindaa/core/App.java
@@ -0,0 +1,13 @@
+package com.tindaa.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
